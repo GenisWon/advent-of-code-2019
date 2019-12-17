090de278de5343f4a075bda1537593d69c7c38ba
Caedmon DelVecchio <bbgenis11@gmail.com>
2019-12-17 01:12:12 -0500

    Day 1 Challenge

---
+++ Day 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Day_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program prgrm = new Program();
+            System.IO.StreamReader file = new System.IO.StreamReader("mass.txt");
+            string strLine;
+            int intCounter = 0;
+            int intMass;
+            int intFuel = 0;
+
+            while ((strLine = file.ReadLine()) != null)
+            {
+                intMass = int.Parse(strLine);
+                while(prgrm.calcFuel(intMass) > 0)
+                {
+                    intMass = prgrm.calcFuel(intMass);
+                    intFuel += intMass;
+                }
+                intCounter++;
+            }
+
+            Console.WriteLine("Fuel needed: ");
+            Console.Write(intFuel);
+        }
+
+        private int calcFuel(int intMass)
+        {
+            return (intMass / 3) - 2;
+        }
+    }
+}
